5aa343f8ad9972a30b6d62b63e1a6fa92d9b1c99
Olivier <naturarerum@yahoo.fr>
2016-12-07 17:25:54 -0500

    07-12-2016

--- vocrusse.py
+++ vocrusse.py
@@ -7,16 +7,18 @@
 # Created:     23-07-2016
 #-------------------------------------------------------------------------------
 # TODO modifier les entetes
+import tkinter as tk
 import random
 import codecs
 
+# Variables Globales
 ficIn = 'D:\\Temp\\russe_vocabulearn.txt'
 
 
 f = codecs.open(ficIn, encoding='utf-8')
 listVoc = []
 for line in f:
-    print(line)
+    #print(line)
     splitted = line.rstrip().split(",")
     listVoc.append(splitted)
     count = 0
@@ -24,16 +26,23 @@ for line in f:
 # print(listVoc)
 
 
-def choix_question():
+def choix_question(langue_choisie):
     """choix aleatoire d un element source ou cible"""
-    # TODO gerer langue source et cible avec des conditions
+
+    value = langue_choisie
     un_element = random.choice(listVoc)
-    langue_source = (un_element[0])
-    langue_cible = (un_element[1])
+    if value == 'Russe':
+        langue_source = (un_element[0])
+    else:
+        langue_source = (un_element[1])
+    print(langue_source)
+    print(un_element)
+    print(value)
     return langue_source
 
 
-resultat = choix_question()
+
+"""cresultat = choix_question()
 print(resultat)
 
 
@@ -46,7 +55,7 @@ def check_reponse(rep):
     return statut
 
 finale = check_reponse("pomme")
-print(finale)
+print(finale)"""
 
 
 
