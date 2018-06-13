03b60b3da9facda35973a3d12a81c8704b3b1f8d
naturarerum <naturarerum@yahoo.fr>
2018-06-12 22:46:25 -0400

    Refactored version presque fonctionnelle

--- vocrusseGui.py
+++ vocrusseGui.py
@@ -1,6 +1,6 @@
 # -------------------------------------------------------------------------------
-# Name:        module2
-# Purpose:
+# Name:        VocRusse 2
+# Purpose:     refactoring into OOP
 #
 # Author:      Olivier
 #
@@ -12,125 +12,152 @@ import tkinter as tk
 from tkinter import ttk
 import random
 import codecs
-
+import sys
+from tkinter import filedialog
 from tkinter import Menu
-from tkinter import messagebox as msBox
-
-import vocrusse
-
-# Main window
-win = tk.Tk()
-win.title("Vocrusse")
-win.iconbitmap(r'C:\Python34\DLLs\pyc.ico')
-
-# Variables Globales
-ficIn = 'D:\\Temp\\russe_vocabulearn.txt'
-count = 0
-
-f = codecs.open(ficIn, encoding='utf-8')
-listVoc = []
-for line in f:
-    splitted = line.rstrip().split(",")
-    listVoc.append(splitted)
-    count += 1
-    print(count)
-
-# Variables Globales
-langue_choisie = tk.StringVar()
-langue_cible = tk.StringVar()
-reponse = tk.StringVar()
-mot_demande = tk.StringVar()
-mot_compare = tk.StringVar()
-value = tk.StringVar()
-resultat = tk.StringVar()
-affichrep = tk.StringVar()
-
-# Création du label liste déroulante choix de langue source
-lbl_choix_langue = ttk.Label(win, text="Langue            : ").grid(column=0, row=0)
-
-# Création du label mot a trouver
-lbl_mot_demande = ttk.Label(win, text="Mot a trouver  : ").grid(column=0, row=1)
-
-# Création du label qui affiche le  mot a trouver
-lbl_mot_demande_affiche = ttk.Label(win, textvariable=mot_demande).grid(column=1, row=1)
-
-# Création du label reponse
-lbl_reponse = ttk.Label(win, text="Réponse           : ").grid(column=0, row=2)
-
-# Création du label qui indique si la reponse est bonne
-lbl_result = ttk.Label(win, textvariable=affichrep).grid(column=1, row=3)
-
-def choix_question(langue):
-
-    """choix aleatoire d un element source ou cible"""
-    value = langue
-    un_element = random.choice(listVoc)
-    if value == 'Russe':
-        langue_source = (un_element[0])
-        langue_cible = (un_element[1])
-        myvar = langue_source
-        myvarcible = langue_cible
-    else:
-        langue_source = (un_element[1])
-        langue_cible = (un_element[0])
-        myvar = langue_source
-        myvarcible = langue_cible
-    mot_demande.set(myvar)
-    mot_compare.set(myvarcible)
-    print(langue_source)
-    print(un_element)
-    return langue_source, langue_cible
+from tkinter import messagebox
 
 
+class AppGui(tk.Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.master.title("Vocabulaire")
+        self.master.iconbitmap(r'C:\Users\GrandMage\PycharmProjects\vocrusse\ressources\Cactus.ico')
+        self.master.columnconfigure(0, weight=1)
+        self.master.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.grid(sticky="NSEW")
+        self.create_widgets()
 
-# Selection de la langue dans la liste déroulante
-def selection_langue(event):
-    langue_choisie = choix_langue.get()
-    return langue_choisie
+    def create_widgets(self):
+        # Class variables
+        self.langue_choisie = tk.StringVar()
+        self.reponse = tk.StringVar()
+        self.mot_demande = tk.StringVar()
+        self.mot_compare = tk.StringVar()
+        self.value = tk.StringVar()
+        self.resultat = tk.StringVar()
+        self.affichrep = tk.StringVar()
+        self.valeurOneVar = tk.StringVar()
+        self.valeurOneVar.set("Ecrivez ici")
 
-# Selection du mot a demander
-def selection_mot():
-    valeur = langue_choisie.get()
-    choix_question(valeur)
+    def read_file(self, filename):
+        fichier = filename
+        count = 0
+        try:
+            f = codecs.open(fichier, encoding='utf-8')
+        except IOError as e:
+            messagebox.showerror("Erreur", "I/O error({0}): {1}".format(e.errno, e.strerror))
+        except:  # handle other exceptions such as attribute errors
+            messagebox.showerror("Unexpected error:", sys.exc_info()[0])
+        listVoc = []
+        for line in f:
+            splitted = line.rstrip().split(",")
+            listVoc.append(splitted)
+            count += 1
+            print(count)
+        return listVoc
+
+        mainFrame = ttk.Frame(self, borderwidth=2, relief="groove")
+        mainFrame.grid(column=0, row=0, sticky="NSEW")
+
+        # Creates a Widget Button for the random selection of a word
+        btn_selection = ttk.Button(mainFrame, text="Selection", command=self.selection_mot).grid(column=0, row=3)
+        valeurOneLabel = tk.Label(mainFrame, text="ValeurOne").grid(column=0, row=0)
+        valeurOneEntry = tk.Entry(mainFrame, textvariable=self.valeurOneVar)
+
+        # Création du label liste déroulante choix de langue source
+        lbl_choix_langue = ttk.Label(mainFrame, text="Langue            : ").grid(column=0, row=0)
+
+        # Création du label mot a trouver
+        lbl_mot_demande = ttk.Label(mainFrame, text="Mot a trouver  : ").grid(column=0, row=1)
+
+        # Création du label qui affiche le  mot a trouver
+        lbl_mot_demande_affiche = ttk.Label(mainFrame, textvariable=self.mot_demande).grid(column=1, row=1)
+
+        # Création du label reponse
+        lbl_reponse = ttk.Label(mainFrame, text="Réponse           : ").grid(column=0, row=2)
+
+    def askopenfile(self):
+        # get filename
+        filename = filedialog.askopenfilename()
+        # return filename
+        return filename
+
+    # Selection du mot a demander
+    def selection_mot(self):
+        valeur = self.langue_choisie.get()
+        self.choix_question(valeur)
+
+    # Selection de la langue dans la liste déroulante
+    def selection_langue(self, event):
+        self.langue_choisie = self.choix_langue.get()
+        return self.langue_choisie
+
+    def check_reponse(self):
+        self.resultat = self.reponse.get()
+        mymot2 = self.mot_compare.get()
+        print("res : ", self.resultat)
+        print(mymot2)
+        if self.resultat == mymot2:
+            statut = 1
+            res = "OK"
+        else:
+            statut = 0
+            res = "NOK"
+        print(statut)
+        self.affichrep.set(res)
+        return statut
+
+    #     """choix aleatoire d un element source ou cible"""
+    def choix_question(self, langue):
+        self.value = langue
+        un_element = random.choice(listVoc)
+        if self.value == 'Russe':
+            self.langue_source = (un_element[0])
+            self.langue_cible = (un_element[1])
+            myvar = self.langue_source
+            myvarcible = self.langue_cible
+        else:
+            self.langue_source = (un_element[1])
+            self.langue_cible = (un_element[0])
+            myvar = self.langue_source
+            myvarcible = self.langue_cible
+        self.mot_demande.set(myvar)
+        self.mot_compare.set(myvarcible)
+        print(self.langue_source)
+        print(un_element)
+        return self.langue_source, self.langue_cible
+
+        # Création du widget liste déroulante choix de langue source
+        self.choix_langue = ttk.Combobox(master, width=12, textvariable=self.langue_choisie)
+        self.choix_langue['values'] = ('Français', 'Russe')
+        self.choix_langue.grid(column=1, row=0)
+        self.choix_langue.current(0)
+        self.choix_langue.focus()
+        self.choix_langue.bind("<<ComboboxSelected>>", self.selection_langue)
+
+        # Création du widget saisie de la réponse
+        self.saisie_reponse = ttk.Entry(master, width=15, textvariable=self.reponse)
+        self.saisie_reponse.grid(column=1, row=2)
+
+        # Création du label qui indique si la reponse est bonne
+        lbl_result = ttk.Label(master, textvariable=self.affichrep).grid(column=1, row=3)
+        self.frame = tk.Frame(self.master)
+        self.initialize()
 
 
-def check_reponse():
-    resultat = reponse.get()
-    mymot2 = mot_compare.get()
-    print("res : ", resultat)
-    print(mymot2)
-    if resultat == mymot2:
-        statut = 1
-        res="OK"
-    else:
-        statut = 0
-        res = "NOK"
-    print(statut)
-    affichrep.set(res)
-    return statut
-
-
-# Création du widget liste déroulante choix de langue source
-choix_langue = ttk.Combobox(win, width=12, textvariable=langue_choisie)
-choix_langue['values'] = ('Français','Russe' )
-choix_langue.grid(column=1, row=0)
-choix_langue.current(0)
-choix_langue.focus()
-choix_langue.bind("<<ComboboxSelected>>", selection_langue)
-
-# Création du widget saisie de la réponse
-saisie_reponse = ttk.Entry(win, width=15, textvariable=reponse)
-saisie_reponse.grid(column=1, row=2)
-
-
-# Création d'un widget bouton pour la selection aleatoire du mot
-btn_selection = ttk.Button(win, text="Selection", command=selection_mot).grid(column=0, row=3)
-
-# Création d'un widget bouton pour la verification de la reponse
-btn_reponse = ttk.Button(win, text="Réponse", command=check_reponse).grid(column=2, row=3)
-
-# =================================================================================#                                                                             #
+# =================================================================================#
 #                                 START GUI                                        #
 #==================================================================================#
 
-win.mainloop()
+
+def main():
+    app = AppGui()
+    app.create_widgets()
+    app.mainloop()
+
+
+if __name__ == '__main__':
+    main()
