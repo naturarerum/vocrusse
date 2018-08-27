ff5fed38558ff4db297b5f5321dec5071708d4ce
naturarerum <naturarerum@yahoo.fr>
2018-08-26 23:43:04 -0400

    Refactored version - reste  logger a faire fonctionner + UI

--- vocrusseGui.py
+++ vocrusseGui.py
@@ -1,12 +1,12 @@
 # -------------------------------------------------------------------------------
-# Name:         VocRusse 1.2.0
+# Name:         VocRusse 1.1.0
 # Purpose:     - refactoring into OOP - first stage (classes)
 #              - ui improvements
 #
 #
 # Author:      Olivier
 #
-# Created:     28--06-2018
+# Created:     28-06-2018
 #
 # -------------------------------------------------------------------------------
 
@@ -20,18 +20,20 @@ from tkinter import filedialog
 from tkinter import messagebox
 
 
-class appGui(Tk):
+class AppGui(Tk):
     def __init__(self, parent):
         Tk.__init__(self, parent)
         self.parent = parent
-        self.title('titre')
+        self.title('VocTrainer')
         self.iconbitmap(r'C:\Users\GrandMage\PycharmProjects\vocrusse\ressources\Cactus.ico')
-        self.geometry('270x146')
+        self.geometry('248x123')
         self.menuBar = Menu(master=self)
         self.filemenu = Menu(self.menuBar, tearoff=0)
-        self.statmenu = Menu(self.menuBar, tearoff=0)
+        self.aboutmenu = Menu(self.menuBar, tearoff=0)
         self.config(menu=self.menuBar)
         self.createWidgets()
+        self.version = '1.1.0'
+
 
     def createWidgets(self):
         # Class variables
@@ -43,29 +45,24 @@ class appGui(Tk):
         self.value = tk.StringVar()
         self.resultat = tk.StringVar()
         self.affichrep = tk.StringVar()
+        self.cpte_reponses_totales = tk.IntVar()
+        self.cpte_reponses_ok = tk.IntVar()
+        self.cpte_reponses_nok = tk.IntVar()
+        self.affiche_score = tk.IntVar()
         self.list_v = []
 
         # Création du label liste déroulante choix de langue source
         lbl_choix_langue = tk.Label(main_frame, text="Matière            : ").grid(row=0)
 
-
         # Adding items to the Menu
         self.menuBar.add_cascade(label="File", menu=self.filemenu)
         self.filemenu.add_command(label="Open", command=self.read_file)
         self.filemenu.add_command(label="Quit", command=self.quit)
-        self.menuBar.add_cascade(label="Stats", menu=self.statmenu)
-
-        # Status bar
-        # status = tk.Label(main_frame, text="Réponse : ").grid(column=0, row=4, sticky=W+E)
-
-        # Creates a Button for the random selection of a word
-        # btn_selection = tk.Button(main_frame, text="Selection", command=self.choix_question).grid(row = 1)
+        self.menuBar.add_cascade(label="About", menu=self.aboutmenu)
+        self.aboutmenu.add_command(label="Version", command=self.affiche_version)
 
         # Creates a Button for the random selection of a word
         btn_selection = tk.Button(main_frame, text="Selection", command=self.choix_question).grid(column=2, row=0)
-
-        # Création d'un bouton pour le choix du fichier
-        #btn_fichier = tk.Button(main_frame, text="Fichier", command=self.read_file).grid(column=2, row=4)
 
         # Création d'un  bouton pour soumettre la réponse
         btn_reponse = tk.Button(main_frame, text="Vérifier", width=7, command=self.check_reponse).grid(column=2, row=3)
@@ -85,7 +82,7 @@ class appGui(Tk):
 
         # Création du widget liste déroulante choix de langue source
         self.choix_langue = ttk.Combobox(main_frame, width=12, textvariable=self.langue_choisie)
-        self.choix_langue['values'] = ('Français', 'Russe')
+        self.choix_langue['values'] = ('Français', 'Russe', 'Addition', 'Multiplication')
         self.choix_langue.grid(column=1, row=0)
         self.choix_langue.current(0)
         self.choix_langue.focus()
@@ -95,8 +92,13 @@ class appGui(Tk):
         self.saisie_reponse = tk.Entry(main_frame, width=15, textvariable=self.reponse)
         self.saisie_reponse.grid(column=1, row=3)
 
-        # Création du label qui indique si la reponse est bonne
-        lbl_result = tk.Label(main_frame, textvariable=self.affichrep).grid(column=0, row=4)
+        # Création de la barre de statut
+        lbl_result = tk.Label(main_frame, relief=tk.SUNKEN, anchor=tk.W, font=('arial', 16, 'normal'), fg='blue',
+                              width=8,
+                              textvariable=self.affichrep).grid(column=0, row=5, columnspan=2, sticky=W)
+        lbl_score = tk.Label(main_frame, relief=tk.SUNKEN, anchor=tk.W, font=('arial', 16, 'normal'), fg='blue',
+                             width=13,
+                             textvariable=self.affiche_score).grid(column=1, row=5, columnspan=2, sticky=E)
 
     def read_file(self):
         print('----read file  Début-----')
@@ -123,6 +125,9 @@ class appGui(Tk):
         print('----Langue choisie Début-----')
         self.langue_choisie = self.choix_langue.get()
         print('Langue choisie : ', self.langue_choisie)
+        self.cpte_reponses_totales = 0
+        self.cpte_reponses_ok = 0
+        self.cpte_reponses_nok = 0
         return self.langue_choisie
 
     def check_reponse(self):
@@ -133,57 +138,49 @@ class appGui(Tk):
         if self.resultat == mymot2:
             statut = 1
             res = "OK"
+            self.cpte_reponses_nok += 1
         else:
             statut = 0
             res = "NOK"
+            self.cpte_reponses_nok += 1
         print(statut)
         self.affichrep.set(res)
+        self.cpte_reponses_totales += 1
         return statut
 
     #     """choix aleatoire d un element source ou cible"""
     def choix_question(self):
+        self.reponse.set('')
+        self.affichrep.set('')
         self.langue_source = []
         self.langue_cible = []
         try:
             un_element = random.choice(self.list_v)
         except IndexError:
-            messagebox.showerror("Error", "Sélectionnez un fichier")
-        if self.langue_choisie == 'Russe':
+            messagebox.showerror("Erreur", "Sélectionnez un fichier")
+        if self.langue_choisie == 'Russe' or 'Addition' or 'Multiplication':
             print('----si russe -----', self.value)
             self.langue_source = (un_element[0])
             self.langue_cible = (un_element[1])
             myvar = self.langue_source
             myvarcible = self.langue_cible
         else:
-            print('----si auitre -----', self.value)
+            print('----si autre -----', self.value)
             self.langue_source = (un_element[1])
             self.langue_cible = (un_element[0])
-            myvar = self.langue_source
-            myvarcible = self.langue_cible
-        # self.mot_demande.set(myvar)
-        # self.mot_compare.set(myvarcible)
         self.mot_demande.set(self.langue_source)
         self.mot_compare.set(self.langue_cible)
         print(self.langue_source)
         print(un_element)
         return self.langue_source, self.langue_cible
 
+    def affiche_version(self):
+        messagebox.showinfo("Version : ")
 
-class StatusBar(tk.Frame):
-    def __init__(self, master):
-        tk.Frame.__init__(self, master)
-        self.variable = tk.StringVar()
-        self.label = tk.Label(self, relief=tk.SUNKEN, anchor=tk.W,
-                              textvariable=self.variable,
-                              font=('arial', 16, 'normal'))
-        self.variable.set('Status Bar')
-        self.label.grid(column=0, row=4, columnspan=2, sticky=W + E)
-        self.grid()
-
-    def clear_status(self):
+    def compte_score(self):
         pass
 
+
 if __name__ == "__main__":
-    app = appGui(None)
-    d = StatusBar(app)
+    app = AppGui(None)
     app.mainloop()
